10258999615ceb0096bc3a5f43c0d0f7b0708042
Jon McClure <jmcclure@politico.com>
2018-04-16 11:19:33 -0400

    configure field names in modeladmin mixin. Adds error handling. closes #1

--- foreignform/mixins.py
+++ foreignform/mixins.py
@@ -6,6 +6,8 @@ from django.core.exceptions import FieldDoesNotExist
 class ForeignFormAdminMixin(object):
     foreignform_field = None
     foreignform_foreign_key = None
+    foreignform_jsonschema_field = 'jsonSchema'
+    foreignform_uischema_field = 'uiSchema'
     change_form_template = 'foreignform/change.html'
     add_form_template = 'foreignform/add.html'
 
@@ -15,8 +17,8 @@ class ForeignFormAdminMixin(object):
 
         if not fk:
             raise FieldDoesNotExist(
-                'Field {} does not exist on model. Check the '
-                'foreignform_foreign_key attribute on your foreignform '
+                'Field "{}" does not exist on model. Check the '
+                '"foreignform_foreign_key" attribute on your foreignform '
                 'ModelAdmin class.'.format(
                     self.foreignform_foreign_key
                 )
@@ -24,8 +26,31 @@ class ForeignFormAdminMixin(object):
 
         extra_context = extra_context or {}
         extra_context['foreignform_field'] = self.foreignform_field
-        extra_context['foreignform_JSONSchema'] = json.dumps(fk.jsonSchema)
-        extra_context['foreignform_UISchema'] = json.dumps(fk.uiSchema)
+
+        try:
+            json_schema = getattr(fk, self.foreignform_jsonschema_field)
+        except AttributeError as error:
+            raise FieldDoesNotExist(
+                'Field "{}" does not exist on foreign-keyed model, {}. Check '
+                'the "foreignform_jsonschema_field" attribute on your '
+                'foreignform ModelAdmin class.'.format(
+                    self.foreignform_jsonschema_field,
+                    self.foreignform_field,
+                )
+            )
+        try:
+            ui_schema = getattr(fk, self.foreignform_uischema_field)
+        except AttributeError as error:
+            raise FieldDoesNotExist(
+                'Field "{}" does not exist on foreign-keyed model, {}. Check '
+                'the "foreignform_uischema_field" attribute on your '
+                'foreignform ModelAdmin class.'.format(
+                    self.foreignform_uischema_field,
+                    self.foreignform_field,
+                )
+            )
+        extra_context['foreignform_JSONSchema'] = json.dumps(json_schema)
+        extra_context['foreignform_UISchema'] = json.dumps(ui_schema)
         return super().change_view(
             request, object_id, form_url, extra_context=extra_context,
         )
