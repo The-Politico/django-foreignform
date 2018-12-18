005ef27c80cefb79a675bd1f40c1c88190004582
Jon McClure <jmcclure@politico.com>
2018-12-17 21:05:54 -0500

    updates widget renderer for django 2.1. bumps to 0.0.8

--- foreignform/widgets.py
+++ foreignform/widgets.py
@@ -3,11 +3,11 @@ from django.utils.safestring import mark_safe
 
 
 class CodeMirrorWidget(widgets.Textarea):
-    def render(self, name, value, attrs=None):
-        if 'class' not in attrs.keys():
-            attrs['class'] = ''
+    def render(self, name, value, attrs=None, renderer=None):
+        if "class" not in attrs.keys():
+            attrs["class"] = ""
 
-        attrs['class'] += ' codemirror-json-editor'
+        attrs["class"] += " codemirror-json-editor"
 
         html = super().render(name, value, attrs)
 
@@ -15,19 +15,19 @@ class CodeMirrorWidget(widgets.Textarea):
 
     class Media:
         js = (
-            'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/codemirror.min.js',  # noqa: E501
-            'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/mode/javascript/javascript.min.js',  # noqa: E501
-            'https://cdnjs.cloudflare.com/ajax/libs/jsonlint/1.6.0/jsonlint.min.js',  # noqa: E501
-            'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/lint/lint.min.js',  # noqa: E501
-            'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/lint/json-lint.min.js',  # noqa: E501
-            'foreignform/js/codemirror.js',
+            "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/codemirror.min.js",  # noqa: E501
+            "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/mode/javascript/javascript.min.js",  # noqa: E501
+            "https://cdnjs.cloudflare.com/ajax/libs/jsonlint/1.6.0/jsonlint.min.js",  # noqa: E501
+            "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/lint/lint.min.js",  # noqa: E501
+            "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/lint/json-lint.min.js",  # noqa: E501
+            "foreignform/js/codemirror.js",
         )
 
         css = {
-            'all': (
-                'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/codemirror.min.css',  # noqa: E501
-                'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/theme/cobalt.min.css',  # noqa: E501
-                'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/lint/lint.min.css',  # noqa: E501
-                'foreignform/css/codemirror.css',
+            "all": (
+                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/codemirror.min.css",  # noqa: E501
+                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/theme/cobalt.min.css",  # noqa: E501
+                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.36.0/addon/lint/lint.min.css",  # noqa: E501
+                "foreignform/css/codemirror.css",
             )
         }
