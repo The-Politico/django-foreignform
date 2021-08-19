5fee251bee943343c85132a9386fbf1544d4746d
Andrew Briz <briz.andrew@gmail.com>
2021-08-19 16:16:43 -0400

    Removeing psycopg2-binary requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-foreignform",
-    version="0.0.8",
+    version="0.0.9",
     description="Dynamic fieldsets for your Django ModelAdmin using JSON Schema and react-jsonschema-form.",
     url="https://github.com/The-Politico/django-foreignform",
     author="POLITICO interactive news",
@@ -24,6 +24,6 @@ setup(
     keywords="",
     packages=find_packages(exclude=["tests", "example"]),
     include_package_data=True,
-    install_requires=["psycopg2-binary"],
+    install_requires=[],
     extras_require={"test": ["pytest"]},
 )
